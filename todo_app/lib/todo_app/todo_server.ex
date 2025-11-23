49058749e24df1a34a9c316916fa6f27691196af
OSHIN  |  おしん <158509127+JaoShiGitHub@users.noreply.github.com>
2025-11-24 06:29:12 +0700

    Delete todo_app directory

--- todo_app/lib/todo_app/todo_server.ex
+++
@@ -1,9 +0,0 @@
-defmodule TodoApp.TodoServer do
-  use GenServer
-
-  def start_link(_) do
-    initial_value = 0
-    GenServer.start_link(__MODULE__, initial_value, name: __MODULE__)
-  end
-
-end
